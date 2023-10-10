b2192c4d83610efbe620bf321971e33db1e46034
Kevin Edmond <135303747+kaedmond24@users.noreply.github.com>
2023-10-10 17:52:19 -0400

    Update main.tf
    Removed mutable ids

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 # configure aws provider
 provider "aws" {
   # Add access key and secret to run terraform apply
-  access_key = " "
-  secret_key = " "
+  access_key = "Enter Access Key"
+  secret_key = "Enter Secret Key"
   region = "us-east-1"
   #profile = "Admin"
 }
@@ -12,8 +12,8 @@ resource "aws_instance" "web_server02" {
   ami = "ami-08c40ec9ead489470"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web_ssh.id]
-  key_name = "pubEC2"
-  subnet_id = "subnet-0427f1f3609b0167c"
+  key_name = "Enter Key Pair Name"
+  subnet_id = "Enter Subnet ID"
   associate_public_ip_address = "true"
 
   user_data = "${file("jenkins_setup.sh")}"
@@ -28,7 +28,7 @@ resource "aws_instance" "web_server02" {
 # create security groups
 
 resource "aws_security_group" "web_ssh" {
-  vpc_id = "vpc-05e4446b7f563f0da"
+  vpc_id = "Enter VPC ID"
   name        = "tf_kura_sg"
   description = "open ssh traffic"
 
